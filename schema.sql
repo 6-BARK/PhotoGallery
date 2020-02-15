84b3abb9e8f381a91f69e2c62619e53b311a45c9
John Roswell Cua <ross.cua@gmail.com>
2020-02-15 15:58:28 -0800

    added users table to sql schema

--- schema.sql
+++ schema.sql
@@ -4,20 +4,42 @@ CREATE DATABASE houses;
 
 USE houses
 
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+)
+
 CREATE TABLE houselisting (
     id int NOT NULL AUTO_INCREMENT,
+    users_id int NOT NULL,
     sqft int(10) NOT NULL,
     bedroom int(10) NOT NULL,
     bathroom int(10) NOT NULL,
     price int(10) NOT NULL,
+    FOREIGN KEY (users_id)
+        REFERENCES users(id)
     PRIMARY KEY (id)
 )
 
 CREATE TABLE images (
     id int NOT NULL AUTO_INCREMENT,
+    users_id int NOT NULL,
     house_id int NOT NULL,
     image_url varchar(255) NOT NULL,
     FOREIGN KEY (house_id)
+        REFERENCES houselisting(id)
+    FOREIGN KEY (users_id)
+        REFERENCES users(id)
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE usersavedhomes (
+    id int NOT NULL AUTO_INCREMENT,
+    users_id int NOT NULL,
+    house_id int NOT NULL,
+    FOREIGN KEY (users_id) 
+        REFERENCES users(id)
+    FOREIGN KEY (house_id) 
         REFERENCES houselisting(id)
     PRIMARY KEY (id)
 )
