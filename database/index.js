3ac4eefa4a92c81a010f9c52dd9f5ebbcf85cc7a
John Roswell Cua <ross.cua@gmail.com>
2020-02-24 12:00:38 -0800

    removed pw

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Roswell00',
+  password: '',
   database: 'houses',
   port: 3306,
 });
