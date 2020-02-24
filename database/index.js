743645e7e179e04a29dde77016ca947a2c740d08
John Roswell Cua <ross.cua@gmail.com>
2020-02-24 12:00:13 -0800

    k6script config

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'Roswell00',
   database: 'houses',
   port: 3306,
 });
